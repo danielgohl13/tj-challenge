d36d11043fc7b80cf59bab182e65f395cdbd92df
Daniel Gohl <danielgohl13@gmail.com>
2020-06-10 15:24:32 -0400

    configuração inicial do express web server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+
+app.use(cors(corsOptions));
+
+//requisição parseamento content-type da application/json
+app.use(bodyParser.json());
+
+//requisição de parseamento do Content-type (diz o tipo de midia que o client recebe)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//rota '/'
+app.get("/", (req, res) => {
+    res.json({ message: "Teste 1"});
+});
+
+//portas definidas esperando requisições
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log('Server rodando na porta ${PORT}.');
+});
