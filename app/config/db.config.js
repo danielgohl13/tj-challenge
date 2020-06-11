a806453ae1b623d77b957cff054eb6c8158dc835
Daniel Gohl <danielgohl13@gmail.com>
2020-06-11 15:27:46 -0400

    remove models add and setup for static data

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "123",
-    DB: "processDB",
+    USER: "danielgohl",
+    PASSWORD: "Herdas13",
+    DB: "ProcessDB",
     dialect: "postgres",
     pool: {
       max: 5,
